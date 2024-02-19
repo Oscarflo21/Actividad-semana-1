48b9f492dad8a7198c8feeefe6f87067c6811dd5
oscarflo21 <oscflorez@hotmail.com>
2024-02-19 12:07:16 -0500

    primer copia

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import Perfil from'./componentes/Perfil';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Mis desarrolladores favoritos son</h1>
+        <Perfil
+          nombre='Brais Moure'
+          pais=' España'        
+          imagen='brais'
+          cargo='Ingeniero de software'
+          empresa='MoureDev'
+          perfil='Es un Youtuber español, fundador de la empresa de desarrollo MoureDev.
+          MoureDev es el reflejo de la ilusión por crecer como profesional dentro de la 
+          industria del desarrollo del software. Como freelance, se ha especializado en 
+          desarrollo de aplicaciones iOS, Android y web'
+          linkedin='https://www.linkedin.com/in/brasimoure/'
+          youtube='https://www.youtube.com/@mouredev'
+          twitter='https://www.twitter.com/Mouredev'
+        />
+        <Perfil
+          nombre='Lucas Moy'
+          pais=' Argentina'        
+          imagen='lucas'
+          cargo='Ingeniero de software'
+          empresa='ATL Academy'
+          perfil='Es un Youtuber Argentino radicado en españa, fundador de ATL Academy.
+          Durante varios años ha compartido sus conocimientos de programación en YouTube.
+          Ha varios cursos y bootcamp meticulosamente para que cualquier persona, sin conocimientos
+          previos, pueda adentrarse de manera amena y eficiente en este fascinante mundo de
+          la programación.'
+          linkedin='https://www.linkedin.com/pub/dir/Lucas/Moy/'
+          youtube='https://www.youtube.com/@atl.academy'
+          twitter='https://twitter.com/LucasMoy_dev/status/1678428356495265794'
+        />
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/componentes/Perfil.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../estilos/Perfil.css';
+
+function Perfil(props){
+    return(
+        <div className='contenedor-perfil'>
+            <img className='imagen-perfil'
+            src={require(`../imagenes/perfil-${props.imagen}.png`)}
+            alt='Foto de desarrollador' />
+            <div className='contenedor-texto-perfil'>
+                <p className='nombre-perfil'>
+                    <strong>{props.nombre}</strong> -- {props.pais}
+                </p>    
+            <p className='cargo-perfil'>
+                {props.cargo} en <strong>{props.empresa}</strong>
+            </p>
+            <p className='texto-perfil'>"{props.perfil}"</p>
+            </div>
+        
+        <button onClick={() => window.open(props.linkedin, '_blank')}>
+        Seguir en LinkedIn
+        </button>
+        <button onClick={() => window.open(props.youtube, '_blank')}>
+        Seguir en YouTube
+        </button>
+        <button onClick={() => window.open(props.twitter, '_blank')}>
+        Seguir en Twitter
+        </button>
+    </div>
+    );
+}
+export default Perfil;
